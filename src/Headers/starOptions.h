b1ac8543eb39b9803bce2364e49b488cd4d0dcbe
Francis McCabe <fmccabe@gmail.com>
2018-06-26 09:35:38 -0700

    rename CAFE to STAR

--- src/Headers/starOptions.h
+++ src/Headers/starOptions.h
@@ -1,5 +1,5 @@
-#ifndef _CAFE_OPTIONS_H_
-#define _CAFE_OPTIONS_H_
+#ifndef _STAR_OPTIONS_H_
+#define _STAR_OPTIONS_H_
 
 #ifndef MAX_SYMB_LEN
 #define MAX_SYMB_LEN 1024
@@ -8,7 +8,6 @@
 #define EXIT_SUCCEED 0    /* Normal exit */
 #define EXIT_FAIL 1        /* Failing exit */
 #define EXIT_ERROR 2      // Error exit
-
 
 #define STAR_LOGFILE "STAR_LOGFILE"
 #define STAR_REPO "STAR_REPO"
