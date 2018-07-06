3230f249e1b7d2f5ff6e45194fed6835927a09dc
Francis McCabe <fmccabe@gmail.com>
2018-07-05 18:45:32 -0700

    Start work on CnC in Star

---
+++ src/BootCompiler/cnc.pl
@@ -0,0 +1,11 @@
+:- module(cnc,[transRule/3]).
+
+:- use_module(wff).
+:- use_module(types).
+:- use_module(canon).
+:- use_module(unify).
+:- use_module(abstract).
+:- use_module(terms).
+
+
+checkRule(Rl)
