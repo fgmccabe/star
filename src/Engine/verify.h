fc86ca52c2432df7774c4a3893a17d4382537e21
Francis McCabe <fmccabe@gmail.com>
2018-07-28 17:06:50 -0700

    Starting work on code verifier

---
+++ src/Engine/verify.h
@@ -0,0 +1,13 @@
+//
+// Created by Francis McCabe on 7/26/18.
+//
+
+#ifndef CAFE_VERIFY_H
+#define CAFE_VERIFY_H
+
+#include "codeP.h"
+#include <assert.h>
+#include "formioP.h"
+#include "labels.h"
+
+#endif //CAFE_VERIFY_H
