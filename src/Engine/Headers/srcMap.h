9d0c95fb6affde1decafcd639e07cde20c3a067c
Francis McCabe <fmccabe@gmail.com>
2018-10-28 18:34:35 +0900

    Start work on source map

--- src/Engine/Headers/srcMap.h
+++ src/Engine/Headers/srcMap.h
@@ -5,4 +5,18 @@
 #ifndef STAR_SRC_MAP_H
 #define STAR_SRC_MAP_H
 
+#include "pkg.h"
+#include "labels.h"
+
+typedef struct _map_entry_ *srcMapPo;
+
+extern packagePo srcMapPkg(srcMapPo mp);
+extern labelPo srcMapFun(srcMapPo mp);
+extern integer srcMapLine(srcMapPo mp);
+extern integer srcMapPc(srcMapPo mp);
+
+extern srcMapPo findInSrcMap(packagePo pkg,integer line);
+
+extern retCode parseSrcMap(char *text,integer length);
+
 #endif //STAR_SRC_MAP_H
