72989aa4de8e5f2232eac96b35fb3dfcbb9fb61e
Francis McCabe <fmccabe@gmail.com>
2019-01-30 12:47:17 -0800

    Added Cmake build instructions for the reference manual.
    A little more on the types chapter

---
+++ Docs/makeinfo.cmake
@@ -0,0 +1,31 @@
+find_program(MAKEINFO makeinfo)
+
+function(add_info nm)
+  if(MAKEINFO)
+    set(info_in ${CMAKE_CURRENT_SOURCE_DIR}/${nm}.texi)
+    set(info_out ${CMAKE_CURRENT_BINARY_DIR}/${nm}.info)
+    set(info_html ${CMAKE_CURRENT_BINARY_DIR}/${nm}.html)
+
+    set(info_deps)
+
+    foreach(v ${ARGV})
+      set(info_deps ${info_deps} ${v}.texi)
+    endforeach()
+
+    add_custom_command(OUTPUT ${info_out}
+      COMMAND ${MAKEINFO} -o ${info_out} ${info_in}
+      DEPENDS ${info_deps} 
+      COMMENT "Creating Info file ${info_out}"
+      VERBATIM)
+
+    add_custom_target(info ALL DEPENDS ${info_out})
+
+    add_custom_command(OUTPUT ${info_html}
+      COMMAND ${MAKEINFO} --html --no-split -o ${info_html} ${info_in}
+      DEPENDS ${info_deps} 
+      COMMENT "Creating HTML file ${info_html}"
+      VERBATIM)
+
+    add_custom_target(html ALL DEPENDS ${info_html})
+  endif(MAKEINFO)
+endfunction(add_info)
