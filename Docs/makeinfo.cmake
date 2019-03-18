8ee4293acfc627d13620e276c53ac23a704991e6
Francis McCabe <fmccabe@gmail.com>
2019-03-17 22:12:39 -0700

    Small fix to build script

--- Docs/makeinfo.cmake
+++ Docs/makeinfo.cmake
@@ -4,7 +4,7 @@ message("Makeinfo = ${MAKEINFO} $ENV{PATH}")
 
 function(add_info nm)
   if(MAKEINFO)
-    set(info_in ${CMAKE_CURRENT_SOURCE_DIR}/${nm}.texi)
+    set(info_texi ${CMAKE_CURRENT_SOURCE_DIR}/${nm}.texi)
     set(info_out ${CMAKE_CURRENT_BINARY_DIR}/${nm}.info)
     set(info_html ${CMAKE_CURRENT_BINARY_DIR}/${nm}.html)
 
@@ -15,7 +15,7 @@ function(add_info nm)
     endforeach()
 
     add_custom_command(OUTPUT ${info_out}
-      COMMAND ${MAKEINFO} -o ${info_out} ${info_in}
+      COMMAND ${MAKEINFO} -o ${info_out} ${info_texi}
       DEPENDS ${info_deps} 
       COMMENT "Creating Info file ${info_out}"
       VERBATIM)
@@ -23,7 +23,7 @@ function(add_info nm)
     add_custom_target(info ALL DEPENDS ${info_out})
 
     add_custom_command(OUTPUT ${info_html}
-      COMMAND ${MAKEINFO} --html --no-split -o ${info_html} ${info_in}
+      COMMAND ${MAKEINFO} --html --no-split -o ${info_html} ${info_texi}
       DEPENDS ${info_deps} 
       COMMENT "Creating HTML file ${info_html}"
       VERBATIM)
