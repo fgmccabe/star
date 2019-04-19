e4516cf5fddd4c344230208e30587ee701d0821e
Francis McCabe <fmccabe@gmail.com>
2019-04-19 15:52:28 -0700

    Check in new top-level driver

---
+++ star-windows.el
@@ -0,0 +1,15 @@
+;;; star-windows.el --- -*- lexical-binding: t; -*-
+
+;;; Copyright (C) 2019 and beyond F.G. McCabe
+
+;;; Create an organization of windows for star ide
+
+
+
+(defun star-window-setup (fr)
+  (let* ((nf (if fr fr (make-frame)))
+	 (top (fr )
+    nf))
+
+(star-window-setup nil)
+
