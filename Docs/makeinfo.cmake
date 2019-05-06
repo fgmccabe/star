8315fd4b391177e1446a924203a7ce2b624115a3
Francis McCabe <fmccabe@gmail.com>
2019-05-05 22:48:58 -0700

    MW on book and WP

--- Docs/makeinfo.cmake
+++ Docs/makeinfo.cmake
@@ -5,8 +5,9 @@ find_program(MAKEINFO makeinfo
 function(add_info nm)
   if(MAKEINFO)
     set(info_texi ${CMAKE_CURRENT_SOURCE_DIR}/${nm}.texi)
-    set(info_out ${CMAKE_CURRENT_BINARY_DIR}/${nm}.info)
-    set(info_html ${CMAKE_CURRENT_BINARY_DIR}/${nm}.html)
+    set(out_info ${CMAKE_CURRENT_BINARY_DIR}/${nm}.info)
+    set(out_html ${CMAKE_CURRENT_BINARY_DIR}/${nm}.html)
+    set(out_pdf ${CMAKE_CURRENT_BINARY_DIR}/${nm}.pdf)
 
     set(info_deps)
 
@@ -14,22 +15,32 @@ function(add_info nm)
       set(info_deps ${info_deps} ${v}.texi)
     endforeach()
 
-    add_custom_command(OUTPUT ${info_out}
-      COMMAND ${MAKEINFO} -o ${info_out} ${info_texi}
+    add_custom_command(OUTPUT ${out_info}
+      COMMAND ${MAKEINFO} -o ${out_info} ${info_texi}
       DEPENDS ${info_deps} 
-      COMMENT "Creating Info file ${info_out}"
+      COMMENT "Creating Info file ${out_info}"
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
       VERBATIM)
 
-    add_custom_target(${nm}.info ALL DEPENDS ${info_out})
+    add_custom_target(${nm}.info ALL DEPENDS ${out_info})
 
-    add_custom_command(OUTPUT ${info_html}
-      COMMAND ${MAKEINFO} --html --no-split -o ${info_html} ${info_texi}
+    add_custom_command(OUTPUT ${out_html}
+      COMMAND ${MAKEINFO} --html --no-split -o ${out_html} ${info_texi}
       DEPENDS ${info_deps} 
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      COMMENT "Creating HTML file ${info_html}"
+      COMMENT "Creating HTML file ${out_html}"
       VERBATIM)
 
-    add_custom_target(${nm}.html ALL DEPENDS ${info_html})
+    add_custom_target(${nm}.html ALL DEPENDS ${out_html})
+
+    add_custom_command(OUTPUT ${out_pdf}
+      COMMAND ${MAKEINFO} --pdf -o ${out_pdf} ${info_texi}
+      DEPENDS ${info_deps} 
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      COMMENT "Creating Pdf file ${out_pdf}"
+      VERBATIM)
+
+    add_custom_target(${nm}.pdf ALL DEPENDS ${out_pdf})
+
   endif(MAKEINFO)
 endfunction(add_info)
