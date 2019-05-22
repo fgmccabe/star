56f76f6716035d86fe274a059ac1b1a987feaa94
Francis McCabe <fmccabe@gmail.com>
2019-05-22 07:15:13 -0700

    Trying to make sure compilation output buffers get removed

--- star-repo.el
+++ star-repo.el
@@ -96,25 +96,30 @@
      :command `(,star-compiler "--stdin" "-r" ,repo "-w" ,dir "--" ,pkg)
      :sentinel
      (lambda (proc event)
-       (star-debug "event %s from %s" event proc)
-       (when (eq 'exit (process-status proc))
-         (unwind-protect
-             ;; Only proceed if `proc' is the same as
-             ;; `star--flymake-proc', which indicates that
-             ;; `proc' is not an obsolete process.
-             ;;
-             (if (with-current-buffer source (eq proc star--flymake-proc))
-                 (with-current-buffer (process-buffer proc)
-		   (let ((error-report
-			  (star-parse-errors source
-					     (process-buffer proc))))
-		     (star-debug "error report from %s is %s" pkg error-report)
-		     (funcall report-fn error-report)))
-               (flymake-log :warning "Canceling obsolete check %s"
-                            proc))
-           ;; Cleanup the temporary buffer used to hold the compiler's output
-           (kill-buffer (process-buffer proc))
-	   ))))
+       (let ((proc-status (process-status proc))
+	     (proc-buffer (process-buffer proc)))
+	 (star-debug "process status %s from %s" proc-status proc)
+	 (when (or (eq 'signal proc-status) (eq 'exit proc-status))
+           (unwind-protect
+               ;; Only proceed if `proc' is the same as
+               ;; `star--flymake-proc', which indicates that
+               ;; `proc' is not an obsolete process.
+               ;;
+               (if (with-current-buffer source (eq proc star--flymake-proc))
+                   (with-current-buffer proc-buffer
+		     (let ((error-report
+			    (star-parse-errors source proc-buffer)))
+		       (star-debug "error report from %s is %s" pkg error-report)
+		       (funcall report-fn error-report)))
+		 (progn
+		   (star-debug "Canceling obsolete check %s"  proc)
+		   (kill-buffer (process-buffer star--flymake-proc))
+		   ))
+             ;; Cleanup the temporary buffer used to hold the compiler's output
+	     (star-debug "kill buffer %s" proc)
+             (kill-buffer proc-buffer)
+	     ))))
+     )
     )
   )
 
@@ -130,8 +135,8 @@
      :sentinel
      (lambda (proc event)
        (let ((proc-status (process-status proc)))
-	 (star-debug "event %s/%s from compiling %s" event proc-status pkg)
-	 (when (or (eq 'exit proc-status) (eq 'closed proc-status))
+	 (star-debug "status %s from compiling %s" proc-status pkg)
+	 (when (or (eq 'exit proc-status) (eq 'signal proc-status))
            (kill-buffer (process-buffer proc))
 	   (message "%s compiling %s" (string-trim event) pkg)))
        )
