adc9bc554d7643b0f47a5cc09ed9af07aa63d9d4
Francis McCabe <fmccabe@gmail.com>
2020-01-08 22:51:45 -0800

    start on local regexp

---
+++ src/Util/Headers/regexpP.h
@@ -0,0 +1,29 @@
+//
+// Created by Francis McCabe on 1/8/20.
+//
+
+#ifndef STAR_REGEXPP_H
+#define STAR_REGEXPP_H
+
+#include "regexp.h"
+
+typedef enum {
+  empty,
+  dot,
+  disj,
+  seq,
+  literal
+} regexpOp;
+
+typedef struct _regexp_ {
+  regexpOp op;
+  union{
+    codePoint chr;
+    struct {
+      regexpPo left;
+      regexpPo right;
+    } choice;
+  } arg;
+} Regexp;
+
+#endif //STAR_REGEXPP_H
