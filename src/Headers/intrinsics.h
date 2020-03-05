ff83a4dba6810fe4b6621ca51c60d95ed9750c77
Francis McCabe <fmccabe@gmail.com>
2020-03-04 22:48:17 -0800

    Move arithmetic intrinsics out
    Until can implement raw arithmetic

--- src/Headers/intrinsics.h
+++ src/Headers/intrinsics.h
@@ -6,7 +6,8 @@
 
 /* Define the standard intrinsics */
 
-intrinsic(_int_plus,"F(ii)i","IAdd","add two integers")
+/*
+ * intrinsic(_int_plus,"F(ii)i","IAdd","add two integers")
 intrinsic(_int_minus,"F(ii)i","ISub","subtract two integers")
 intrinsic(_int_times,"F(ii)i","IMul","multiply two integers")
 intrinsic(_int_div,"F(ii)i","IDiv","divide two integers")
@@ -20,7 +21,7 @@ intrinsic(_flt_mod,"F(ff)f","FMod","modulo remainder")
 
 intrinsic(_int_abs,"F(i)i","IAbs","integer absolute value")
 intrinsic(_flt_abs,"F(f)f","FAbs","float absolute value")
-
+*/
 intrinsic(_int_eq,"F(ii)l","IEq","integer equality")
 intrinsic(_int_lt,"F(ii)l","ILt","integer less than")
 intrinsic(_int_ge,"F(ii)l","IGe","integer greater or equal")
@@ -28,7 +29,7 @@ intrinsic(_int_ge,"F(ii)l","IGe","integer greater or equal")
 intrinsic(_flt_eq,"F(fff)l","FEq","float equality")
 intrinsic(_flt_lt,"F(ff)l","FLt","float less than")
 intrinsic(_flt_ge,"F(ff)l","FGe","float greater or equal")
-
+/*
 intrinsic(_band,"F(ii)i","BAnd","bitwise and two integers")
 intrinsic(_bor,"F(ii)i","BOr","bitwise or two integers")
 intrinsic(_bxor,"F(ii)i","BXor","bitwise xor two integers")
@@ -36,3 +37,4 @@ intrinsic(_blsl,"F(ii)i","BLsl","logical left shift")
 intrinsic(_blsr,"F(ii)i","BLsr","logical right shift")
 intrinsic(_basr,"F(ii)i","BAsr","arithmetic right shift")
 intrinsic(_bnot,"F(i)i","BNot","bitwise negate number")
+*/
