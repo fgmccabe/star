6e8184b727e1b611d5a016af5f2bb5688db43fd7
Francis McCabe <fmccabe@gmail.com>
2020-07-02 09:12:58 -0700

    small edit

---
+++ src/Engine/Headers/starP.h
@@ -0,0 +1,8 @@
+//
+// Created by Francis McCabe on 7/1/20.
+//
+
+#ifndef STAR_STARP_H
+#define STAR_STARP_H
+
+#endif //STAR_STARP_H
---
+++ src/Jit/X86_64/Headers/jitOps.h
@@ -0,0 +1,8 @@
+//
+// Created by Francis McCabe on 7/1/20.
+//
+
+#ifndef STAR_JITOPS_H
+#define STAR_JITOPS_H
+
+#endif //STAR_JITOPS_H
