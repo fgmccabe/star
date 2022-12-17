902eecc9393226fe279916723ea3e361368daa2f
Francis McCabe <fmccabe@gmail.com>
2022-12-16 22:10:33 -0800

    CAS instructions

--- src/Jit/ARM64/Tests/cas.s
+++ src/Jit/ARM64/Tests/cas.s
@@ -17,11 +17,41 @@ _start:	mov X0, #1     // 1 = StdOut
         mov     X16, #1     // Service command code 1 terminates this program
         svc     0           // Call MacOS to terminate the program
 
-1:	bl	1b
-	bl	2f
-	blr	X12
-	br	X13
-2:	brk	#1234
+//	casb	w6, w8, [x3]
+	
+//	casab	w10, w3, [X2]
+//	casab	w10, w3, [sp]
 
+//	casalb	w10, w3, [x5]
+
+//	caslb	w10, w3, [x0]
+
+//	cash	w6, w8, [x3]
+	
+//	casah	w10, w3, [X2]
+//	casah	w10, w3, [sp]
+
+//	casalh	w10, w3, [x5]
+
+//	caslh	w10, w3, [x0]
+
+	;; casp	x6, x7, x8,x9, [x3]
+	
+	;; caspa	x10,x11, x2,x3, [X2]
+	;; caspa	x10,x11, x2,x3, [sp]
+
+	;; caspal	x10,x11, x2,x3, [x5]
+
+	;; caspl	x10,x11, x2,x3, [x0]
+
+	cas	x6, x8, [x3]
+
+	casa	x10, x3, [X2]
+	casa	x10, x3, [sp]
+
+	casal	x10, x3, [x5]
+	casl	x10, x3, [x0]
+	
+	
 helloworld:	.ascii "Hi there\n"
 	.end
