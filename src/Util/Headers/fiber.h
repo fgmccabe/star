6425df997be33c51a2391d3d8eabc72add017f08
Francis McCabe <fmccabe@gmail.com>
2022-12-19 21:21:36 -0800

    Thinking about making a fiber api to use

--- src/Util/Headers/fiber.h
+++ src/Util/Headers/fiber.h
@@ -5,4 +5,18 @@
 #ifndef STAR_FIBER_H
 #define STAR_FIBER_H
 
+#include "integer.h"
+
+typedef struct fiber_record_ *fiberPo;
+
+
+typedef enum {
+  suspended,
+  active,
+  moribund
+} FiberState;
+
+fiberPo createFiber(integer size);
+void dropFiber(fiberPo fiber);
+
 #endif //STAR_FIBER_H
