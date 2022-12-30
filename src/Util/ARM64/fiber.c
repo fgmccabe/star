6a75d2356ab8938cde818661cf90dc4aeb109631
Francis McCabe <fmccabe@gmail.com>
2022-12-29 19:53:10 -0800

    More fiber experiments

--- src/Util/ARM64/fiber.c
+++ src/Util/ARM64/fiber.c
@@ -1,3 +1,12 @@
 //
 // Created by Francis McCabe on 12/28/22.
 //
+#include "fiberP.h"
+#include "buddy.h"
+
+fiberPo createFiber(fiberFun fn, void *cl,integer size){
+
+}
+void dropFiber(fiberPo fiber);
+
+void switchFiber(fiberPo next,void *val);
