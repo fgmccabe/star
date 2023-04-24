3ddc33933207d26c0d73131a194d2c8b58406e21
Francis McCabe <fmccabe@gmail.com>
2023-04-23 20:15:52 -0700

    Fix some util warnings

--- src/Util/Headers/unistr.h
+++ src/Util/Headers/unistr.h
@@ -29,14 +29,14 @@ typedef enum {
   unknownEncoding
 } ioEncoding;
 
-typedef enum{
-  Cc,  Cf,  Cn,  Co,  Cs,
-  Ll,  Lm,  Lo,  Lt,  Lu,
-  Mc,  Me,  Mn,
-  Nd,  Nl,  No,
-  Pc,  Pd,  Pe,  Pf,  Pi,  Po,  Ps,
-  Sc,  Sk,  Sm,  So,
-  Zl,  Zp,  Zs, Other
+typedef enum {
+  Cc, Cf, Cn, Co, Cs,
+  Ll, Lm, Lo, Lt, Lu,
+  Mc, Me, Mn,
+  Nd, Nl, No,
+  Pc, Pd, Pe, Pf, Pi, Po, Ps,
+  Sc, Sk, Sm, So,
+  Zl, Zp, Zs, Other
 } UniCharCategory;
 
 /* Is character a legal codePoint char? */
@@ -88,7 +88,7 @@ integer countCodePoints(const char *src, integer start, integer end);
 integer advanceCodePoint(char *src, integer start, integer end, integer count);
 codePoint codePointAt(const char *src, integer pt, integer end);
 codePoint nextCodePoint(const char *src, integer *start, integer end);
-codePoint prevCodePoint(const char *src, integer *start, integer end);
+codePoint prevCodePoint(const char *src, integer *start);
 retCode nxtPoint(const char *src, integer *start, integer end, codePoint *code);
 retCode prevPoint(const char *src, integer *pos, codePoint *code);
 integer backCodePoint(const char *src, integer from, integer count);
@@ -96,39 +96,40 @@ integer backCodePoint(const char *src, integer from, integer count);
 int codePointSize(codePoint pt);
 
 integer uniStrLen(const char *s);
-integer uniNStrLen(const char *s,integer max);
+integer uniNStrLen(const char *s, integer max);
 retCode uniCpy(char *dest, integer len, const char *src);
 retCode uniNCpy(char *dest, integer len, const char *src, integer sLen);
 retCode uniMove(char *dest, integer len, const char *src, integer sLen);
-comparison unicodeCmp(const char *s1, integer l1,const char *s2,integer l2);
+comparison unicodeCmp(const char *s1, integer l1, const char *s2, integer l2);
 comparison uniCmp(const char *s1, const char *s2);
 comparison uniNCmp(const char *s1, integer l1, const char *s2, integer l2);
-logical uniSame(const char *s1,integer l1, const char *s2,integer l2);
+logical uniSame(const char *s1, integer l1, const char *s2, integer l2);
 retCode appendCodePoint(char *dest, integer *pos, integer len, codePoint ch);
 retCode uniAppend(char *dest, integer *pos, integer len, char *src);
 retCode uniNAppend(char *dest, integer *pos, integer len, char *src, integer sLen);
 retCode uniReverse(char *dest, integer len);
-char * uniDupl(char *str,integer len);
+char *uniDupl(char *str, integer len);
 
 integer uniIndexOf(const char *s, integer len, integer from, codePoint c);
 integer uniLastIndexOf(const char *s, integer len, codePoint c);
 
 long uniSearch(const char *src, integer len, integer start, const char *tgt, integer tlen);
 codePoint uniSearchDelims(char *s, integer len, char *t);
-char * uniDuplicate(const char *s);
+char *uniDuplicate(const char *s);
 void uniDestroy(char *s);
 logical uniIsLit(const char *s1, const char *s2);
 logical uniIsLitPrefix(const char *s1, const char *s2);
 logical uniIsPrefix(const char *s1, integer len1, const char *s2, integer len2);
-integer uniHash(const char * name);
-integer uniNHash(const char * name, long len);
+logical uniIsSuffix(const char *s1, integer len1, const char *s2, integer len2);
+integer uniHash(const char *name);
+integer uniNHash(const char *name, long len);
 retCode uniLower(const char *s, integer sLen, char *d, integer dLen);
 retCode uniTrim(const char *s, integer sLen, char *front, char *trail, char *out, integer outLen);
 logical uniIsTrivial(const char *s, integer len);
 
-logical sameBytes(const byte *s1,integer l1,const byte* s2,integer l2);
+logical sameBytes(const byte *s1, integer l1, const byte *s2, integer l2);
 retCode byteMove(byte *dest, integer len, const byte *src, integer sLen);
-logical sameWords(const uint32 *s1,integer l1,const uint32* s2,integer l2);
+logical sameWords(const uint32 *s1, integer l1, const uint32 *s2, integer l2);
 retCode wordMove(uint32 *dest, uint32 len, const uint32 *s, uint32 sLen);
 integer byteHash(const byte *data, long len);
 integer wordHash(const uint32 *data, long len);
