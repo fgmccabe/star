bb6e0675e2a05534d826cbf42dfb831059a6cab3
Francis McCabe <fmccabe@gmail.com>
2024-08-20 07:39:36 -0700

    Starting (again) on a fiber library
    Prerequisite to JIT

---
+++ src/Util/Headers/fiber.h
@@ -0,0 +1,27 @@
+//
+// Created by Francis McCabe on 8/19/24.
+//
+
+#ifndef STAR_FIBER_H
+#define STAR_FIBER_H
+
+#include "retcode.h"
+
+// Fiber structure. Allow code to switch between coroutines
+
+typedef enum {
+  active,
+  suspended,
+  moribund
+} fiberStatus;
+
+typedef struct fiber_record_ *fiberPo;
+typedef retCode (*fiberFun)(fiberPo thisFiber, void *cl);
+
+fiberPo newFiber(fiberFun runner);
+
+retCode suspendFiber(fiberPo thisFiber, fiberStatus msg, void *data);
+retCode resumeFiber(fiberPo fiber, fiberStatus msg, void *data);
+retCode retireFiber(fiberPo thisFbier, fiberStatus ret, void *data);
+
+#endif //STAR_FIBER_H
---
+++ src/Util/Headers/fiberP.h
@@ -0,0 +1,40 @@
+//
+// Created by Francis McCabe on 8/19/24.
+//
+
+#ifndef STAR_FIBERP_H
+#define STAR_FIBERP_H
+
+#include "fiber.h"
+#include "objectP.h"
+#include <setjmp.h>
+
+typedef struct {
+  jmp_buf env;                          // Jump buffer for switching
+  void *cl;                             // Client data
+  void *memory;                         // This fiber's stack memory
+  integer size;                         // And its size
+} FiberObjectRec;
+
+typedef struct fiber_record_ {
+  ObjectRec object;                     /* object level of the fiber structure */
+  FiberObjectRec fiber;
+} FiberObjRecord;
+
+typedef struct {
+} FiberClassPart;
+
+typedef struct fiber_class_ {
+  ObjectClassRec objectPart;
+  FiberClassPart fiberPart;
+} FiberClassRec;
+
+extern FiberClassRec FiberClass;
+
+#ifdef VERIFY_OBJECT
+#define O_FIBER(c) ((fiberPo)(checkCast((c),fiberClass)))
+#else
+#define O_FIBER(c) ((fiberPo)(c))
+#endif
+
+#endif //STAR_FIBERP_H
