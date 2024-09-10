641d7f575a9442e9c76b43327a6257b13ad00ee5
Francis McCabe <fmccabe@gmail.com>
2024-09-10 16:37:13 -0700

    MW on setting up a test

--- src/Jit/ARM64/Tests/regset.c
+++ src/Jit/ARM64/Tests/regset.c
@@ -9,8 +9,23 @@
 #include "macros.h"
 #include "test_infra.h"
 
+static registerMap mapFromArray(armReg regs[], int count){
+  registerMap set = emptyRegSet();
+
+  for(int ix=0;ix<count;ix++)
+    set = addReg(set, regs[ix]);
+
+  return set;
+}
+
 retCode test_set1(){
   assemCtxPo ctx = createCtx();
+
+  armReg someRegs[] = {X0, X3, X4, X5, X10, X20, X17};
+
+  registerMap map = mapFromArray(someRegs,NumberOf(someRegs));
+
+  saveRegisters(ctx,map);
   
   uint8 tgt[] = {};
 
