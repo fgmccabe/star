b8511eca1cc1bc5beee285aef5e4912e0c39191f
Francis McCabe <fmccabe@gmail.com>
2025-06-22 22:20:14 -0700

    Dont use X18 for jitted code

--- src/Jit/ARM64/Assem/macros.c
+++ src/Jit/ARM64/Assem/macros.c
@@ -6,7 +6,7 @@
 
 registerMap defltAvailRegSet() {
   return 1u << X0 | 1u << X1 | 1u << X2 | 1u << X3 | 1u << X4 | 1u << X5 | 1u << X6 | 1u << X7 | 1u << X8 | 1u << X9 |
-         1u << X10 | 1u << X18;
+         1u << X10;
 }
 
 registerMap emptyRegSet() {
