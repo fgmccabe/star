592ab3fd2bfb6a1d097aff1b3e1c5bff927b8fa7
Francis McCabe <fmccabe@gmail.com>
2025-12-05 22:59:06 -0800

    Got bitmap sets working.

--- src/Util/Headers/set.h
+++ src/Util/Headers/set.h
@@ -4,6 +4,7 @@
 #include "logical.h"
 #include "retcode.h"
 #include "config.h"
+#include "ooio.h"
 
 typedef struct set_record_ *setPo;
 
@@ -15,7 +16,11 @@ void deleteSet(setPo s);
 retCode addToSet(setPo set, int32 k);
 retCode removeFromSet(setPo set, int32 k);
 logical inSet(setPo set, int32 k);
+logical setIsEmpty(setPo set);
 
 retCode processSet(setPo set, setElProc proc, void *cl);
 
+retCode showSet(ioPo out,setPo set);
+
 #endif
+
--- src/Util/set.c
+++ src/Util/set.c
@@ -1,79 +1,168 @@
+#include <assert.h>
+
 #include "setP.h"
 #include "pool.h"
 #include "ooio.h"
 #include "utils.h"
 #include <stdlib.h>
+#include <arm/param.h>
 
 static poolPo setPool = Null;
 
-static void initSets(){
-  if(setPool == Null){
+static void initSets() {
+  if (setPool == Null) {
     setPool = newPool(sizeof(SetRecord), 64);
   }
 }
 
-setPo createEmptySet(int32 min){
+setPo createSet(int32 min) {
   initSets();
 
   setPo set = allocPool(setPool);
   set->count = 0;
   set->min = min;
-  set->count = 0;
   set->data = Null;
 
   return set;
 }
 
-void deleteSet(setPo s){
-  if(s->data!=Null)
+void deleteSet(setPo s) {
+  if (s->data != Null)
     free(s->data);
   freePool(setPool, s);
 }
 
-retCode addToSet(setPo set, int32 k){
-  int32 max = set->count*64+set->min;
-  if(k>=set->min && k<max){
-    int32 base = k-set->min;
-    int32 el = base>>6;
-    int32 mask = 1u<<(((unsigned)el)&63);
+static int32 align64(int32 x) {
+  return (x + 64) & (~63);
+}
+
+retCode addToSet(setPo set, int32 k) {
+  int32 max = set->count * 64 + set->min;
+  if (k >= set->min && k < max) {
+    int32 base = k - set->min;
+    int32 el = base >> 6;
+    uinteger mask = 1ul << (((uinteger) base) & 63);
     set->data[el] |= mask;
     return Ok;
-  } else{
-    if(k<set->min){
+  }
+  if (set->data == Null) {
+    int32 mx = align64(k);
+    set->count = 1;
+    set->min = mx - 64;
+    assert(k>=set->min && k<mx);
+    set->data = malloc(sizeof(uinteger) * set->count);
+    for (int32 ix = 0; ix < set->count; ix++)
+      set->data[ix] = 0;
+    return addToSet(set, k);
+  }
 
-    } else{
-      
+  if (k < set->min) {
+    int32 newBase = align64(k-64);
+    int32 oldMax = set->count * 64 + set->min;
+    int32 newCount = (oldMax - newBase) / 64;
+
+    assert(k>=newBase && k<newBase+64);
+
+    uinteger *newData = (uinteger *) malloc(sizeof(uinteger) * newCount);
+    for (int32 ix = 0; ix < newCount-set->count; ix++) {
+      newData[ix] = 0;
     }
+    for (int32 ix = 0; ix < set->count; ix++) {
+      newData[ix+(newCount-set->count)] = set->data[ix]; // We have to copy the old data across
+    }
+    free(set->data);
+    set->data = newData;
+    set->count = newCount;
+    set->min = newBase;
+    return addToSet(set, k);
   }
-  return Ok;
-}
-retCode removeFromSet(setPo set, int32 k);
 
-logical inSet(setPo set, int32 k){
-  int32 max = set->count*64+set->min;
-  if(k<set->min || k>=max || set->data==Null)
-    return False;
-  else{
-    int32 base = k-set->min;
-    int32 el = base>>6;
-    int32 mask = 1u<<(((unsigned)el)&63);
-    return (set->data[el]&mask)==mask;
+  assert(k>=set->min+set->count*64);
+  int32 newCount = align64(k - set->min) / 64;
+  uinteger *newData = (uinteger *) malloc(sizeof(uinteger) * newCount);
+  for (int32 ix = 0; ix < set->count; ix++) {
+    newData[ix] = set->data[ix];
   }
+  for (int32 ix = set->count; ix < newCount; ix++) {
+    newData[ix] = 0;
+  }
+  free(set->data);
+  set->data = newData;
+  set->count = newCount;
+  return addToSet(set, k);
 }
 
-retCode processSet(setPo set, setElProc proc, void *cl){
-  if(set->data==Null)
+retCode removeFromSet(setPo set, int32 k) {
+  if (set->data != Null) {
+    int32 max = set->count * 64 + set->min;
+    if (k < set->min || k >= max)
+      return Error; // Never there
+    int32 base = k - set->min;
+    int32 el = base >> 6;
+    uinteger mask = 1ul << ((uinteger) (base & 63u));
+    set->data[el] &= ~mask;
     return Ok;
-  else{
-    retCode ret = Ok;
-    int32 max = set->count*64+set->min;
-    for(int32 ix=set->min;ret==Ok && ix<max;ix++){
-      int32 base = ix-set->min;
-      int32 el = base>>6;
-      int32 mask = 1u<<(((unsigned)el)&63);
-      if((set->data[el]&mask)==mask)
-        ret = proc(set,ix,cl);
-    }
+  }
+  return Error;
+}
+
+logical setIsEmpty(setPo set) {
+  if (set->data == Null)
+    return True;
+  else {
+    for (int32 ix = 0; ix < set->count; ix++)
+      if (set->data[ix] != 0)
+        return False;
+    return True;
+  }
+}
+
+logical inSet(setPo set, int32 k) {
+  int32 max = set->count * 64 + set->min;
+  if (k < set->min || k >= max || set->data == Null)
+    return False;
+  int32 base = k - set->min;
+  int32 el = base >> 6;
+  uinteger mask = 1ul << (((uinteger) base) & 63u);
+  return (set->data[el] & mask) == mask;
+}
+
+retCode processSet(setPo set, setElProc proc, void *cl) {
+  if (set->data == Null)
+    return Ok;
+  retCode ret = Ok;
+  int32 max = set->count * 64 + set->min;
+  for (int32 ix = set->min; ret == Ok && ix < max; ix++) {
+    int32 base = ix - set->min;
+    int32 el = base >> 6;
+    uinteger mask = 1ul << (((uinteger) base) & 63u);
+    if ((set->data[el] & mask) == mask)
+      ret = proc(set, ix, cl);
+  }
+  return ret;
+}
+
+typedef struct {
+  ioPo out;
+  logical showComma;
+} showInfo;
+
+static retCode showSetEl(setPo set, int32 el, void *cl) {
+  showInfo *info = (showInfo *) cl;
+
+  retCode ret = outMsg(info->out, "%s%d", (info->showComma ? "," : ""), el);
+  info->showComma = True;
+  return ret;
+}
+
+retCode showSet(ioPo out, setPo set) {
+  if (set->data == Null)
+    return outMsg(out, "∅");
+  else {
+    outMsg(out, "{");
+    showInfo info = {.out = out, .showComma = False};
+    retCode ret = processSet(set, showSetEl, &info);
+    outMsg(out, "}%_");
     return ret;
   }
 }
