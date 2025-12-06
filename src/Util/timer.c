0f8ee9ffaea6d9f87c5f000d62aee92f09ec653c
Francis McCabe <fmccabe@gmail.com>
2025-12-06 15:27:19 -0800

    Got the timer tests working reliably
    The issue was in the tests, not in the timer code.

--- src/Util/timer.c
+++ src/Util/timer.c
@@ -8,12 +8,12 @@
 #include <sys/time.h>
 #include <sys/errno.h>
 #include <math.h>
-#include <pthread.h>
 #include "utils.h"
 #include "timer.h"
 #include "pool.h"
 #include "ooio.h"
 #include <assert.h>
+#include <stdio.h>
 
 static poolPo timerPool = Null;
 
@@ -26,17 +26,15 @@ typedef struct timer_struct {
   timePo next;
 } TimerStruct;
 
-static pthread_once_t once = PTHREAD_ONCE_INIT;
-
 static timePo timeQ = Null;
 static void timerWakeUp(int sig);
-static retCode setInterrupt();
+static retCode setInterruptHandler();
 
 static void initAlarms(void) {
   if (timerPool == Null) {
     timerPool = newPool(sizeof(TimerStruct), 16);
     timeQ = Null;
-    setInterrupt();
+    setInterruptHandler();
   }
 }
 
@@ -47,110 +45,113 @@ static logical cmpTime(struct timeval *a, struct timeval *b) {
     return a->tv_sec >= b->tv_sec;
 }
 
-retCode setInterrupt(){
+retCode setInterruptHandler() {
+  outMsg(logFile, "set up interrupt handler\n%_");
+
   // Set up the signal handler
   struct sigaction act;
   act.sa_handler = timerWakeUp;
   act.sa_flags = 0;
 
- tryAgain:
+tryAgain:
   sigemptyset(&act.sa_mask);
 
   sigaction(SIGALRM, &act, Null);
 
-  switch(errno){
-  case 0:
-    break;
-  case EFAULT:
-    outMsg(logFile,"EFAULT in sigaction\n%_");
-    return Error;
-  case EINVAL:
-    outMsg(logFile,"EINVAL in sigaction\n%_");
-    return Error;
-  case EINTR:
-    goto tryAgain;
-  default:
-    outMsg(logFile,"%d in segaction\n%_",errno);
-    return Error;
-  }     
+  switch (errno) {
+    case 0:
+      break;
+    case EFAULT:
+      outMsg(logFile, "EFAULT in sigaction\n%_");
+      return Error;
+    case EINVAL:
+      outMsg(logFile, "EINVAL in sigaction\n%_");
+      return Error;
+    case EINTR:
+      goto tryAgain;
+    default:
+      outMsg(logFile, "%d in segaction\n%_",errno);
+      return Error;
+  }
   return Ok;
 }
-  
+
 static retCode setupTimerInterrupt(struct timeval *when) {
   struct itimerval period;
   struct timeval now;
 
- tryAgain:
+tryAgain:
   gettimeofday(&now, Null);
-  
+
   period.it_value.tv_sec = when->tv_sec - now.tv_sec;
   period.it_value.tv_usec = when->tv_usec - now.tv_usec;
 
-  if (period.it_value.tv_usec < 0) {  /* -ve microseconds */
+  if (period.it_value.tv_usec < 0) {
+    /* -ve microseconds */
     period.it_value.tv_usec += MICROS;
     period.it_value.tv_sec--;
   }
 
-  period.it_interval.tv_usec = 0;     // Single shot timer
+  period.it_interval.tv_usec = 0; // Single shot timer
   period.it_interval.tv_sec = 0;
 
-  //  outMsg(logFile,"period secs: %d, micro %d\n%_",period.it_value.tv_sec,period.it_value.tv_usec);
-
-  if(period.it_value.tv_sec<0 || period.it_value.tv_sec==0 && period.it_value.tv_usec<=0)
+  if (period.it_value.tv_sec < 0 || period.it_value.tv_sec == 0 && period.it_value.tv_usec <= 0)
     return Interrupt;
 
   setitimer(ITIMER_REAL, &period, Null);
 
-  switch(errno){
-  case 0:
-    return Ok;
-  case EINVAL:{
-    outMsg(logFile,"EINVAL in setitimer\n%_");
-    return Error;
-  }
-  case EINTR:
-    goto tryAgain;
-  default:
-    outMsg(logFile,"%d in setitimer\n%_",errno);
-    return Error;
+  switch (errno) {
+    case 0:
+      return Ok;
+    case EINVAL: {
+      outMsg(logFile, "EINVAL in setitimer\n%_");
+      return Error;
+    }
+    case EINTR:
+      goto tryAgain;
+    default:
+      outMsg(logFile, "%d in setitimer\n%_",errno);
+      return Error;
   }
 }
 
 static void clearTimerInterrupt() {
-  struct itimerval zero = {.it_value={.tv_usec=0, .tv_sec=0}, .it_interval={.tv_sec=0, .tv_usec=0}};
+  struct itimerval zero = {.it_value = {.tv_usec = 0, .tv_sec = 0}, .it_interval = {.tv_sec = 0, .tv_usec = 0}};
   setitimer(ITIMER_REAL, &zero, Null);
 }
 
 static void processQ() {
   struct timeval now;
+  clearTimerInterrupt(); // clear any existing timer interrupt
 
-  tryAgain:
+tryAgain:
   gettimeofday(&now, Null);
-  
+
   timePo q = timeQ;
-  //  outMsg(logFile,"process timer Q %s \n%_",timeQLen(q));
-  
-  while(q != Null) {
-    if(q->onWakeup!=Null){
+
+  while (q != Null) {
+    if (q->onWakeup != Null) {
       if (cmpTime(&now, &q->when)) {
-        q->onWakeup(q->cl);
-	q->onWakeup = Null;
-      }
-      else{
-	switch(setupTimerInterrupt(&q->when)){
-	case Ok:
-	  return;
-	case Interrupt:
-	  goto tryAgain;
-	case Error:
-	default:
-	  outMsg(logFile,"error in reset timer\n%_");
-      }
+        timeFun onWakeup = q->onWakeup;
+        q->onWakeup = Null;
+        void *cl = q->cl;
+        onWakeup(cl);
+
+        continue;
+      } else {
+        switch (setupTimerInterrupt(&q->when)) {
+          case Ok:
+            return;
+          case Interrupt:
+            goto tryAgain;
+          case Error:
+          default:
+            outMsg(logFile, "error in reset timer\n%_");
+        }
       }
     }
     q = q->next;
   }
-  clearTimerInterrupt(); // Only get here if no timers remain
 }
 
 static void timerWakeUp(int sig) {
@@ -172,11 +173,12 @@ static timePo insertTimer(struct timeval *when, timePo entry, timePo q) {
   if (q == Null) {
     entry->next = Null;
     return entry;
-  } else if(q->onWakeup==Null){
+  } else if (q->onWakeup == Null) {
     timePo next = q->next;
     freePool(timerPool, q);
-    return insertTimer(when,entry,next);
-  } else if (cmpTime(&q->when, when)) { // new one is younger
+    return insertTimer(when, entry, next);
+  } else if (cmpTime(&q->when, when)) {
+    // new one is younger
     entry->next = q;
     return entry;
   } else {
@@ -186,7 +188,7 @@ static timePo insertTimer(struct timeval *when, timePo entry, timePo q) {
 }
 
 retCode setupAlarm(struct timeval when, timeFun onWakeup, void *cl) {
-  pthread_once(&once, initAlarms);
+  initAlarms();
 
   timePo entry = (timePo) allocPool(timerPool);
 
@@ -194,6 +196,8 @@ retCode setupAlarm(struct timeval when, timeFun onWakeup, void *cl) {
   entry->onWakeup = onWakeup;
   entry->cl = cl;
   entry->next = Null;
+
+  clearTimerInterrupt(); // Clear any existing timers
 
   timeQ = insertTimer(&when, entry, timeQ);
   processQ();
