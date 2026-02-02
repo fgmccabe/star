649723a7f3de8e216a0546bbb56e205e4c983212
Francis McCabe <fmccabe@gmail.com>
2026-02-02 08:57:10 -0800

    Migrate cell escapes

--- src/Escapes/assigns.c
+++ src/Escapes/assigns.c
@@ -8,18 +8,42 @@
 #include "assigns.h"
 #include "cellP.h"
 
-ReturnStatus g__cell(enginePo P) {
-  pshVal(P, (termPo) newCell(processHeap(P), popVal(P)));
-  return Normal;
+ValueReturn s__cell(enginePo P, termPo vl)
+{
+  return normalReturn((termPo)newCell(processHeap(P),vl));
 }
 
-ReturnStatus g__get(enginePo P) {
-  pshVal(P, getCell(C_CELL(popVal(P))));
-  return Normal;
+ReturnStatus g__cell(enginePo P)
+{
+  ValueReturn ret = s__cell(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
 }
 
-ReturnStatus g__assign(enginePo P) {
-  cellPo Cell = C_CELL(popVal(P));
-  setCell(Cell, popVal(P));
-  return Normal;
+ValueReturn s__get(enginePo P, termPo cl)
+{
+  return normalReturn(getCell(C_CELL(cl)));
+}
+
+ReturnStatus g__get(enginePo P)
+{
+  ValueReturn ret = s__get(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
+
+ValueReturn s__assign(enginePo P, termPo c, termPo v)
+{
+  cellPo Cell = C_CELL(c);
+  setCell(Cell, v);
+  return normalReturn(voidEnum);
+}
+
+ReturnStatus g__assign(enginePo P)
+{
+  termPo cell = popVal(P);
+  termPo vl = popVal(P);
+  ValueReturn ret = s__assign(P, cell, vl);
+  pshVal(P, ret.value);
+  return ret.status;
 }
