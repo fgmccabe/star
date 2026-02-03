9cf93320e5b3a61d7839407712506881e1e8987d
Francis McCabe <fmccabe@gmail.com>
2026-02-02 20:44:42 -0800

    Migrate unicode apis

--- src/Escapes/unicode.c
+++ src/Escapes/unicode.c
@@ -7,324 +7,425 @@
 #include "unicode.h"
 #include "errorCodes.h"
 
-ReturnStatus g__uniCodeCategory(enginePo P) {
-  codePoint ch = (codePoint) charVal(popVal(P));
+ValueReturn s__uniCodeCategory(enginePo P, termPo c) {
+  codePoint ch = (codePoint) charVal(c);
 
-  pshVal(P, makeInteger(uniCharCategory(ch)));
-  return Normal;
+  return normalReturn(makeInteger(uniCharCategory(ch)));
+}
+
+ReturnStatus g__uniCodeCategory(enginePo P) {
+  ValueReturn ret = s__uniCodeCategory(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
+
+ValueReturn s__isCcChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
+
+  return normalReturn(isCcChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isCcChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isCcChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isCcChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isCfChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isCfChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isCfChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isCfChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isCfChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isCnChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isCnChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isCnChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isCnChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isCnChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isCoChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isCoChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isCoChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isCoChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isCoChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isCsChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isCsChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isCsChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isCsChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isCsChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isLlChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isLlChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isLlChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isLlChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isLlChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isLmChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isLmChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isLmChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isLmChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isLmChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isLoChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isLoChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isLoChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isLoChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isLoChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isLtChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isLtChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isLtChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isLtChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isLtChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isLuChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isLuChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isLuChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isLuChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isLuChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isMcChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isMcChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isMcChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isMcChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isMcChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isMeChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isMeChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isMeChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isMeChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isMeChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isMnChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isMnChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isMnChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isMnChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isMnChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isNdChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isNdChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isNdChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isNdChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isNdChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isNlChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isNlChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isNlChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isNlChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isNlChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isNoChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isNoChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isNoChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isNoChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isNoChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isPcChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isPcChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isPcChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isPcChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isPcChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isPdChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isPdChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isPdChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isPdChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isPdChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isPeChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isPeChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isPeChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isPeChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isPeChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isPfChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isPfChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isPfChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isPfChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isPfChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isPiChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isPiChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isPiChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isPiChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isPiChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isPoChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isPoChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isPoChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isPoChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isPoChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isPsChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isPsChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isPsChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isPsChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isPsChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isScChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isScChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isScChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isScChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isScChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isSkChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isSkChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isSkChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isSkChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isSkChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isSmChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isSmChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isSmChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isSmChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isSmChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isSoChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isSoChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isSoChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isSoChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isSoChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isZlChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isZlChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isZlChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isZlChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isZlChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isZpChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isZpChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isZpChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isZpChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isZpChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isZsChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isZsChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isZsChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+  ValueReturn ret = s__isZsChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
+}
 
-  termPo Rs = (isZsChar(ch) ? trueEnum : falseEnum);
+ValueReturn s__isLetterChar(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
-  pshVal(P, Rs);
-  return Normal;
+  return normalReturn(isLetterChar(ch) ? trueEnum : falseEnum);
 }
 
 ReturnStatus g__isLetterChar(enginePo P) {
-  codePoint ch = charVal(popVal(P));
-
-  termPo Rs = (isLetterChar(ch) ? trueEnum : falseEnum);
-
-  pshVal(P, Rs);
-  return Normal;
+  ValueReturn ret = s__isLetterChar(P, popVal(P));
+  pshVal(P, ret.value);
+  return ret.status;
 }
 
-ReturnStatus g__digitCode(enginePo P) {
-  codePoint ch = charVal(popVal(P));
+ValueReturn s__digitCode(enginePo P, termPo c) {
+  codePoint ch = charVal(c);
 
   if (isNdChar(ch)) {
-    pshVal(P,makeInteger(digitValue(ch)));
-    return Normal;
+    return normalReturn(makeInteger(digitValue(ch)));
   } else {
-    pshVal(P,eINVAL);
-    return Abnormal;
+    return abnormalReturn(eINVAL);
   }
 }
 
-ValueReturn s__codePoint(enginePo P, termPo c){
+ReturnStatus g__digitCode(enginePo P) {
+  termPo cc = popVal(P);
+  ValueReturn ret = s__digitCode(P, cc);
+  pshVal(P, ret.value);
+  return ret.status;
+}
+
+ValueReturn s__codePoint(enginePo P, termPo c) {
   codePoint ch = (codePoint) charVal(c);
   return normalReturn(makeInteger(ch));
 }
 
 ReturnStatus g__codePoint(enginePo P) {
-  ValueReturn ret = s__codePoint(P,popVal(P));
-  pshVal(P,ret.value);
+  ValueReturn ret = s__codePoint(P, popVal(P));
+  pshVal(P, ret.value);
   return ret.status;
 }
 
-
-ValueReturn s__int2chr(enginePo P, termPo c){
+ValueReturn s__int2chr(enginePo P, termPo c) {
   codePoint ch = (codePoint) integerVal(c);
   return normalReturn(makeChar(ch));
 }
 
 ReturnStatus g__int2chr(enginePo P) {
-  ValueReturn ret = s__int2chr(P,popVal(P));
-  pshVal(P,ret.value);
+  ValueReturn ret = s__int2chr(P, popVal(P));
+  pshVal(P, ret.value);
   return ret.status;
 }
-
